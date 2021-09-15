09f375a24b9caf8fc343f07a40f2dcf6250d340d
Kyle Bascomb <kylebascomb@gmail.com>
2021-09-15 09:39:19 -0700

    Redid Navigation Bar

--- client/src/components/layout/NavigationBar.js
+++ client/src/components/layout/NavigationBar.js
@@ -1,56 +1,67 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
-import { Typography } from '@material-ui/core';
+import Box from '@material-ui/core/Box';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+        marginRight: theme.spacing(6),
+    },
+}));
 
 const NavigationBar = ({ auth, onClick }) => {
-    const [click, setClick] = useState(false);
 
-    const handleClick = () => setClick(!click);
-    const closeMobileMenu = () => setClick(false);
+    const classes = useStyles();
 
 
     return (
-        <>
-            <nav className="navbar">
-                <div className="navbar-container">
-                    <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-                        <Typography variant='h4'>Week Forward</Typography>
-                    </Link>
-                    <div className='menu-icon' onClick={handleClick}>
-                        <i className={click ? 'fa fa-times' : 'fa fa-bars'} />
-                    </div>
-                </div>
-                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                    <li className='nav-item'>
-                        {auth ? (
-                            <Link to='/calendar' className='nav-links' onClick={closeMobileMenu}>
-                                My Calendar
-                            </Link>
-                        ) : (
-                            <Link to='/login' className='nav-links' onClick={closeMobileMenu}>
-                                My Calendar
-                            </Link>
-                        )
-                        }
-                    </li>
-                    {auth ? (
-                        <li className='nav-item'>
-                            <Link to='/login' className='nav-links' onClick={onClick}>
-                                Logout
-                            </Link>
-                        </li>
-                    ) : (<li className='nav-item'>
-                        <Link to='/login' className='nav-links' onClick={closeMobileMenu}>
-                            Login
-                        </Link>
-                    </li>)
-                    }
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar >
+                    <Button align="left" edge="start" color="inherit" href="/" >
+                        <Typography variant="h6" className={classes.title} >Week Ahead</Typography>
+                    </Button>
+                    <Box style={{ flex: 1 }} />
 
-                </ul>
-            </nav>
-        </>
-    )
+                    {auth ? (
+                        <Button variant="outlined" color="inherit" className={classes.menuButton} href="/calendar" >
+                            My Calendar
+                        </Button>
+                    ) : (
+                        <Button variant="outlined" color="inherit" className={classes.menuButton} href="/login" >
+                            My Calendar
+                        </Button>
+                    )}
+
+                    {auth ? (
+                        <Button variant="outlined" color="inherit" className={classes.menuButton} onClick={onClick} href="/login" >
+                            Logout
+                        </Button>
+                    ) : (
+                        <Button variant="outlined" color="inherit" className={classes.menuButton}  href="/login" >
+                            Login
+                        </Button>
+                    )}
+
+
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+
+
 }
 
 export default NavigationBar;
